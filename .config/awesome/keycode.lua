eac8082f53f8b5dd2cd6ad913b9ec5fd11ad8633
YurixAnd <yurixand@gmail.com>
2016-05-09 20:36:27 +0300

    новый файл:    ../../.config/awesome/keycode.lua 	новый файл:    ../../.config/awesome/keydoc.lua 	новый файл:    ../../.config/awesome/themes/gamez/background_one.png

---
+++ .config/awesome/keycode.lua
@@ -0,0 +1,39 @@
+-- Button scan codes
+-- Mouse
+left_button = 1
+right_button = 3
+wheel_button = 2
+wheel_up_button = 4
+wheel_down_button = 5
+up_button = 8
+down_button = 9
+
+-- Keyboard
+key_J = "#44"
+key_K = "#45"
+key_N = "#57"
+key_M = "#58"
+key_F = "#41"
+key_R = "#27"
+key_L = "#46"
+key_C = "#54"
+key_W = "#25"
+key_Q = "#24"
+key_H = "#43"
+key_Tab = "#23"
+key_Tilda = "#49"
+key_U = "#30"
+key_I = "#31"
+key_T = "#28"
+key_P = "#33"
+key_O = "#32"
+key_X = "#53"
+key_Space = "#65"
+key_Return = "#36"
+key_Left = "#113"
+key_Right = "#114"
+key_Esc = "#9"
+key_PrtScn = "#107"
+key_F1 = "#67"
+
+
